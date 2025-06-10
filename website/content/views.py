8b5c66a31ba2a66ba60a7908de3d740628796f46
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-06-10 18:54:55 +0530

    Created two class based views for creation and viewing the posts

--- website/content/views.py
+++ website/content/views.py
@@ -1,2 +1,17 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
+from .models import Posts
+from .forms import PostsModelForm
+from django.urls import reverse_lazy
 
+class ContentCreateView(CreateView):
+    model = Posts
+    form_class = PostsModelForm
+    template_name = "content/content_creation.html"
+    success_url = reverse_lazy("content_feed")
+
+class ContentFeedListView(ListView):
+    model = Posts
+    template_name = "content/content_feed.html"
+    context_object_name = "posts"
