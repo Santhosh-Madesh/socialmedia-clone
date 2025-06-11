2fcdf6d6b503f5ac92f85863ccfee486a11c4ce1
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-06-11 10:50:56 +0530

    Added url patterns

--- website/content/urls.py
+++ website/content/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ContentCreateView, ContentFeedListView
+from .views import ContentCreateView, ContentFeedListView, CommentCreateView
 
 urlpatterns = [
-    path("",ContentFeedListView.as_view(),name="content_feed"),
-    path("create/",ContentCreateView.as_view(),name="content_create")
+    path("", ContentFeedListView.as_view(), name="content_feed"),
+    path("create/", ContentCreateView.as_view(), name="content_create"),
+    path("comment/<int:pk>/", CommentCreateView.as_view(), name="comment"),
 ]
