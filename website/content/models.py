1888bcfdf19b7bfe7f827bb1745177b969e348f5
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-06-11 12:26:51 +0530

    Modifed the posts model to have an extra field called 'likes'

--- website/content/models.py
+++ website/content/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Posts(models.Model):
     picture = models.ImageField(default="media/default.jpg" ,null=True, blank=True)
     caption = models.CharField(null=True, blank=True)
+    likes = models.IntegerField(null=True, blank=True, default=0)
 
 class Comments(models.Model):
     posts = models.ForeignKey(Posts, on_delete=models.CASCADE)
