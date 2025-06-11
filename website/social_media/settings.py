3e19dc4c324ba55d824cc74b74e00ef02091cb5e
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-06-11 15:11:22 +0530

    Added the app path of the user_management

--- website/social_media/settings.py
+++ website/social_media/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'content.apps.UserManagementConfig',
     'content.apps.ContentConfig',
     'django.contrib.admin',
     'django.contrib.auth',
