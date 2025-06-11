4f7a04764f6696f22d287d6cfc70c868e702bbe4
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-06-11 12:28:14 +0530

    Added urlpattern for the like function based view

--- website/content/urls.py
+++ website/content/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import ContentCreateView, ContentFeedListView, CommentCreateView
+from . import views
 
 urlpatterns = [
     path("", ContentFeedListView.as_view(), name="content_feed"),
     path("create/", ContentCreateView.as_view(), name="content_create"),
     path("comment/<int:pk>/", CommentCreateView.as_view(), name="comment"),
+    path("like/<int:pk>/", views.like, name="like")
 ]
