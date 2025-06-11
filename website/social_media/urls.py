0e4b3daaac7f7104a2d787b152b4fee7cfd3b71c
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-06-11 15:11:58 +0530

    Added the url path for the user_management urls

--- website/social_media/urls.py
+++ website/social_media/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path("", include("user_management.urls")),
     path("content/", include('content.urls')),
     path('admin/', admin.site.urls),
 ]
