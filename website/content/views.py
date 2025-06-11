5fe6f75e399aa6b2acf52027d08ff0ee7393e322
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-06-11 10:50:33 +0530

    Created a Create view to create an object for the model comments

--- website/content/views.py
+++ website/content/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
-from .models import Posts
-from .forms import PostsModelForm
+from .models import Posts, Comments
+from .forms import PostsModelForm, CommentModelForm
 from django.urls import reverse_lazy
 
 class ContentCreateView(CreateView):
@@ -15,3 +15,17 @@ class ContentFeedListView(ListView):
     model = Posts
     template_name = "content/content_feed.html"
     context_object_name = "posts"
+
+class CommentCreateView(CreateView):
+    model = Comments
+    form_class = CommentModelForm
+    template_name = "content/comment.html"
+    success_url = reverse_lazy("content_feed")
+
+    def form_valid(self, form, **kwargs):
+        instance = form.save(commit=False)
+        pk = self.kwargs.get("pk")
+        instance.posts = Posts.objects.get(id = pk)
+        form = instance
+        form.save()
+        return redirect("content_feed")
