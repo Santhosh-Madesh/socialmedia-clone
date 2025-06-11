0328a3b36d7c847b52cf2de8cc7e735dced80bea
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-06-11 10:49:51 +0530

    Created a ModelForm for the model comments

--- website/content/forms.py
+++ website/content/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Posts
+from .models import Posts, Comments
 
 class PostsModelForm(forms.ModelForm):
     class Meta:
         model = Posts
         exclude = [""]
+
+class CommentModelForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        exclude = ["posts"]
