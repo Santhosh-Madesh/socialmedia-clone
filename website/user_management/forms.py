9f1a23c067383fe517f14a4ec7cfaf3f296354be
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-06-12 19:48:59 +0530

    Created a ModelForm for the model Profile

--- website/user_management/forms.py
+++ website/user_management/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django import forms
+from .models import Profile
 
 class SignUpModelForm(forms.ModelForm):
     class Meta:
@@ -16,4 +17,13 @@ class LoginModelForm(forms.ModelForm):
         fields = ["username", "password"]
         widget = {
             "password":forms.PasswordInput(),
+        }
+
+class ProfileModelForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ["user"]
+        widget = {
+            "age":forms.IntegerField(min_value=15),
+            "bio":forms.Textarea(),
         }
