c6a998324d38779c18e6e199cc783e4b73322f29
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-06-12 16:06:09 +0530

    Created url patterns for the auth views

--- website/user_management/urls.py
+++ website/user_management/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .auth import SignUpPageView, LoginPageView
+from .auth import SignUpPageView, LoginPageView, LogoutPageView
+from . import auth
 
 urlpatterns=[
     path("signup/", SignUpPageView.as_view(), name="signup"),
     path("login/", LoginPageView.as_view(), name="login"),
+    path("logout/", auth.logoutPage, name="logout")
 ]
