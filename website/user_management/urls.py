6f3c2875e4336115109539b7e3fe1ce0cf165335
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-06-12 19:50:04 +0530

    Created url pattern for the dashboard view

--- website/user_management/urls.py
+++ website/user_management/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .auth import SignUpPageView, LoginPageView, LogoutPageView
 from . import auth
+from .views import ProfileDetailView
+from . import views
 
 urlpatterns=[
     path("signup/", SignUpPageView.as_view(), name="signup"),
     path("login/", LoginPageView.as_view(), name="login"),
-    path("logout/", auth.logoutPage, name="logout")
+    path("logout/", auth.logoutPage, name="logout"),
+    path("dashboard/", views.dashboard, name="dashboard"),
 ]
