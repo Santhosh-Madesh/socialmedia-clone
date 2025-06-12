f36c28485cec35ee6799685b699908f915890e94
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-06-12 19:49:39 +0530

    Created a dashboard view

--- website/user_management/views.py
+++ website/user_management/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import FormView
+from .models import Profile
+from .forms import ProfileModelForm
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+    
+@login_required
+def dashboard(request):
+    if request.method == "POST":
+        form = ProfileModelForm(request.POST)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            return redirect("dashboard")
+    profile = Profile.objects.filter(user=request.user)
+    if not profile:
+        form = ProfileModelForm()
+        return render(request, "user_management/dashboard.html", {"form":form})
+    else:
+        profile = Profile.objects.get(user=request.user)
+        return render(request, "user_management/dashboard.html", context={"profile":profile})
+        
+    
+    
+    
